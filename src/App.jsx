e4b1575a19b3c85b243a025d96cde17ca20760f4
1967cooder <silvanalindholm@hotmail.com>
2025-09-09 10:50:17 +0300

    Välitetään data componentti diedosto App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
-const App = () => {
+const App = (props) => {
+  const {notes} = props
   return (
   <>
-    <p>Hello world</p>
+    <p>Notes</p>
+    <ul>
+      <li>
+        {notes[0].content}
+      </li>
+      <li>
+        {notes[1].content}
+      </li>
+      <li>
+        {notes[2].content}
+      </li>
+    </ul>
   </>
 )
 }
